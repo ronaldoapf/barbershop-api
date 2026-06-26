ab5c8b156e2ff0302b7150fc2ca4151ab66e21b7
ronaldoapf <ronaldo.alves.1997@gmail.com>
2026-06-26 11:51:10 -0300

    feat: configure Swagger UI and global ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, transform: true, forbidNonWhitelisted: true }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Barbershop API')
+    .setDescription('API de gerenciamento de barbearia')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, config));
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
