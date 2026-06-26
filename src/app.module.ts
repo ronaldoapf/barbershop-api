6410071c571b2d9d7b5a542a2389f1d81dc68257
ronaldoapf <ronaldo.alves.1997@gmail.com>
2026-06-26 11:45:20 -0300

    feat: add PrismaService and global PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './shared/infrastructure/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
